258c024f96a5e5ab101ffdeeec2f0a8c9f038265
Ricardo_M_Lu <576470574@qq.com>
2021-12-26 21:26:15 +0800

    训练页面

--- src/views/teacher/exercise/Index.vue
+++ src/views/teacher/exercise/Index.vue
@@ -1,12 +1,112 @@
 <template>
-  <div>
-    <h1>训练管理</h1>
+  <div class="container">
+  <h4 class="title">训练管理</h4>
+    <el-row>
+      <el-col :span="8">
+        <el-checkbox-group v-model="selectedValue" size="small">
+          <el-checkbox-button
+            v-for="item in list"
+            :key="item.value"
+            :label="item.value"
+            @click="handleClick(item.value)"
+            >{{ item.label }}
+          </el-checkbox-button>
+        </el-checkbox-group>
+      </el-col>
+    </el-row>
+    <el-form :inline="true" :model="form" class="search-form">
+      <el-row justify="between">
+        <el-col :span="8">
+          <el-form-item label="考试名称">
+            <el-input v-model="form.name" placeholder="Approved by"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+           <el-form-item label="考试编号">
+            <el-input v-model="form.id" placeholder="Approved by"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="8">
+          <el-form-item label="考试编号">
+            <el-select v-model="form.region" placeholder="Activity zone">
+              <el-option label="Zone one" value="shanghai"></el-option>
+              <el-option label="Zone two" value="beijing"></el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary">搜索</el-button>
+        </el-col>
+      </el-row>
+    </el-form>
+    <exercise-table></exercise-table>
   </div>
 </template>
 
-<script setup>
-
+<script>
+import { reactive, toRef, toRefs } from "vue";
+import exerciseTable from './components/ExerciseTable.vue';
+const selectButtons = [
+  {
+    label: "单选",
+    value: 1,
+  },
+  {
+    label: "单选",
+    value: 2,
+  },
+  {
+    label: "单选",
+    value: 3,
+  },
+  {
+    label: "单选",
+    value: 4,
+  },
+  {
+    label: "单选",
+    value: 5,
+  },
+];
+export default {
+  name: "exercise_index",
+  components: {
+    exerciseTable
+  },
+  setup() {
+    const stateSelect = reactive({
+      selectedValue: [1],
+      list: selectButtons,
+    });
+    function handleClick(value) {
+      stateSelect.selectedValue = [value];
+    }
+    const form = reactive({
+      name: null, // 考试名称
+      id: null, // 考试编号
+    });
+    return {
+      ...toRefs(stateSelect),
+      form,
+      handleClick,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+.container {
+  padding: 10px;
+  box-shadow: 0 0 5px #d0d3d4;
+  border-radius: 4px;
+  .title {
+    text-align: left;
+    margin: 10px 0 20px 20px;
+  }
+  .search-form {
+    margin: 20px 0;
+  }
+}
 </style>
---
+++ src/views/teacher/exercise/components/ExerciseTable.vue
@@ -0,0 +1,66 @@
+<template>
+  <el-table :data="tableData" style="width: 100%">
+    <el-table-column prop="date" label="Date" width="180" />
+    <el-table-column prop="name" label="Name" width="180" />
+    <el-table-column prop="address" label="Address" />
+    <el-table-column fixed="right" label="Operations" width="120">
+      <template #default>
+        <el-button type="text" size="small" @click="handleClick"
+          >Detail</el-button
+        >
+        <el-button type="text" size="small">Edit</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+  <div class="pagination-container">
+    <el-pagination background layout="prev, pager, next" :total="1000"></el-pagination>
+  </div>
+</template>
+
+<script>
+import { toRefs, reactive } from 'vue'
+let tableData = [
+    {
+        date: '2016-05-03',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+        date: '2016-05-02',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+        date: '2016-05-04',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+        date: '2016-05-01',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+    },
+]
+export default {
+    setup() {
+        const state = reactive({
+            tableData
+        })
+        function handleClick() {
+            
+        }
+        return {
+            ...toRefs(state),
+            handleClick
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.pagination-container {
+    display: flex;
+    justify-content: right;
+    margin-top: 10px;
+}
+</style>
